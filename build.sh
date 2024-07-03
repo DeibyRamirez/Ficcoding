d93c660a5e456d8d921758e7f9af7651c8c1d924
DeibyRamirez <122913779+DeibyRamirez@users.noreply.github.com>
2024-07-03 14:43:16 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,23 +2,30 @@
 
 # Instalar gimme si no está instalado
 if ! command -v gimme &> /dev/null; then
+    echo "Instalando gimme..."
     curl -sL -o gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
     chmod +x gimme
     export PATH=$PATH:$(pwd)
 fi
 
-# Configurar la versión de Go que necesitas
-eval "$(GIMME_GO_VERSION='1.17.x' gimme)"  # Asegúrate de reemplazar '1.17.x' con la versión de Go necesaria
+# Configurar la versión de Go necesaria
+eval "$(GIMME_GO_VERSION='1.17.x' gimme)"
 
 # Resto del script para Flutter y construcción de la aplicación web
 # Instalar Flutter, configurar y construir la aplicación web
 if [ ! -d "$HOME/flutter" ]; then
+    echo "Clonando Flutter..."
     git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
 fi
 
 export PATH="$PATH:$HOME/flutter/bin"
 flutter --version
 
+echo "Configurando Flutter para web..."
 flutter config --enable-web
+
+echo "Obteniendo dependencias de Flutter..."
 flutter pub get
+
+echo "Construyendo la aplicación web Flutter..."
 flutter build web
