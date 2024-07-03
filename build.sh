8e81dec0a52f5ee3552bf2ceefdc7a8438e75260
DeibyRamirez <122913779+DeibyRamirez@users.noreply.github.com>
2024-07-03 14:08:50 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Instalar Flutter si no está instalado
+# Instalar gvmo si no está instalado
+if ! command -v gvmo &> /dev/null; then
+    curl -sSL https://raw.githubusercontent.com/mitranim/gvmo/master/install.sh | bash
+    export PATH=$PATH:$HOME/.gvmo/bin
+fi
+
+# Configurar la versión de Go que necesitas
+gvmo use 1.17.x
+
+# Resto del script para Flutter y construcción de la aplicación web
+# Instalar Flutter, configurar y construir la aplicación web
 if [ ! -d "$HOME/flutter" ]; then
     git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
 fi
 
 export PATH="$PATH:$HOME/flutter/bin"
-
-flutter --version  # Asegúrate de tener la versión de Flutter que necesitas
+flutter --version
 
 flutter config --enable-web
 flutter pub get
